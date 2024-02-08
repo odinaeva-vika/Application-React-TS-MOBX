4cc5c1c331b4f27988ddd5d5162930d6553ff1e0
Victoria <121200782+odinaeva-vika@users.noreply.github.com>
2024-02-08 04:54:20 +0300

    search

--- src/feuteres/Search/index.tsx
+++ src/feuteres/Search/index.tsx
@@ -1,14 +1,18 @@
 import { FC } from "react";
 import { observer } from "mobx-react-lite";
 import NewsStore from "../../stores/NewsStore";
+import TextField from "@mui/material/TextField";
 
 const Search: FC = observer(() => {
   return (
     <div>
-      <input
-        type="text"
+      <TextField
         value={NewsStore.keywords}
         onChange={NewsStore.handleSearch}
+        id="outlined-basic"
+        label="Поиск"
+        variant="outlined"
+        size="small"
       />
     </div>
   );
